0fb67973d9cb184373af53523dd1bb5a0a422c3f
rasyidrafi <rasyid.rafi@yahoo.com>
2022-05-21 17:05:51 +0700

    Init Done Homepage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Container from "./components/Container";
+import Navigation from "./components/Navigation";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Navigation />
+      <div style={{ height: "90%" }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </div>
+    </Container>
   );
 }
 
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,68 @@
+import { useState } from 'react';
+import { Nav, NavItem, NavLink, Navbar, NavbarBrand, NavbarToggler, Collapse } from 'reactstrap';
+import { Link, NavLink as NavLinkRouter } from "react-router-dom";
+
+export default () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <Navbar
+            style={{ height: "10%" }}
+            expand="md"
+            dark>
+            <NavbarBrand>
+                <Link to="/" className='brand'>
+                    Rasyid Rafi
+                </Link>
+            </NavbarBrand>
+            <NavbarToggler
+                onClick={() => setIsOpen(!isOpen)}
+            />
+            <Collapse navbar isOpen={isOpen} className="justify-content-center">
+                <Nav navbar>
+                    <NavItem>
+                        <NavLink>
+                            <NavLinkRouter to="/" className={({ isActive }) =>
+                                isActive ? 'brand-link brand-active' : 'brand-link'
+                            }>
+                                Home
+                            </NavLinkRouter>
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink>
+                            <NavLinkRouter to="/profile" className={({ isActive }) =>
+                                isActive ? 'brand-link brand-active' : 'brand-link'
+                            }>
+                                Profile
+                            </NavLinkRouter>
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink>
+                            <NavLinkRouter to="/portfolio" className={({ isActive }) =>
+                                isActive ? 'brand-link brand-active' : 'brand-link'
+                            }>
+                                Portfollio
+                            </NavLinkRouter>
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink>
+                            <NavLinkRouter to="/contact" className={({ isActive }) =>
+                                isActive ? 'brand-link brand-active' : 'brand-link'
+                            }>
+                                Contact
+                            </NavLinkRouter>
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href='http://blog.rasyidrafi.my.id' className="brand-link">
+                            Blog
+                        </NavLink>
+                    </NavItem>
+                </Nav>
+            </Collapse>
+        </Navbar>
+    )
+}
